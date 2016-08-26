4ec1591e19a6c773c9bac5bbb7f2b83cff8b66b2
Norbert Bartos <norbertbartos9@gmail.com>
2016-08-26 11:05:32 +0200

    Rearrange cart items by dragging

--- cartLogic.js
+++ cartLogic.js
@@ -2,18 +2,34 @@ $(document).ready( function () {
 
 	var cartList = $('#cart-list');
 	var draggedElement;
+	var dropPosition;
 
 	$('.item').on('dragstart', function (ev) { dragstart(ev); });
-	$(cartList).on('dragover', function (ev) { dragover(ev); });
 	$(cartList).on('drop', function (ev) { drop(ev); });
 
 	function dragstart (e) {
+
+		$(cartList).on('dragover', function (ev) { dragover(ev); });
 		e.originalEvent.dataTransfer.setData("item", e.target.id);
 		draggedElement = e.target;
+
 	}
 
 	function dragover (e) {
+
 		e.preventDefault();
+
+		var oldDropPosition = dropPosition;
+		dropPosition = e.currentTarget;
+
+		if ($(dropPosition).hasClass('in-cart')) {
+
+			$(cartList).off('dragover');
+			$(oldDropPosition).removeClass('dropzone');
+			$(dropPosition).addClass('dropzone');
+
+		}
+
 	}
 
 	function drop (e) {
@@ -26,17 +42,23 @@ $(document).ready( function () {
 		if(itemInCart.length) {
 
 			if ($(draggedElement).hasClass('in-cart')) {
-				console.log('changing the order - no increment');
+
+				$(draggedElement).insertBefore($(dropPosition));
+				$(dropPosition).removeClass('dropzone');
+
 			} else {
 				console.log('multiple item - increment');
 			}
 
 		} else {
 
-			console.log('new item');
 			$(e.target).append($('#' + data).clone(true));
-			$(e.target).find('#' + data).toggleClass('in-cart');
-			
+
+			var element = $(e.target).find('#' + data);
+
+			$(element).toggleClass('in-cart');
+			$(element).on('dragover', function (ev) { dragover(ev); });
+
 		}
 
 	}
